1fafc6aef4a1530a663004169612f51706ad781a
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-09-03 23:23:10 -0500

    We add last code

--- proyecto.sql
+++ proyecto.sql
@@ -50,5 +50,24 @@ CREATE TABLE IF NOT EXISTS public.tren (
 	CONSTRAINT tren_pkey PRIMARY KEY (id) 
 )
 
+-- Otra creación de tablas
+
+CREATE TABLE Person(
+    BusinessEntityID INT NOT NULL,
+    PersonType char(2) NOT NULL,
+    NameStyle "NameStyle" NOT NULL CONSTRAINT "DF_Person_NameStyle" DEFAULT (false),
+    Title varchar(8) NULL,
+    FirstName "Name" NOT NULL,
+    MiddleName "Name" NULL,
+    LastName "Name" NOT NULL,
+    Suffix varchar(10) NULL,
+    EmailPromotion INT NOT NULL CONSTRAINT "DF_Person_EmailPromotion" DEFAULT (0),
+    AdditionalContactInfo XML NULL, -- XML("AdditionalContactInfoSchemaCollection"),
+    Demographics XML NULL, -- XML("IndividualSurveySchemaCollection"),
+    rowguid uuid NOT NULL CONSTRAINT "DF_Person_rowguid" DEFAULT (uuid_generate_v1()), -- ROWGUIDCOL
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_Person_ModifiedDate" DEFAULT (NOW()),
+    CONSTRAINT "CK_Person_EmailPromotion" CHECK (EmailPromotion BETWEEN 0 AND 2),
+    CONSTRAINT "CK_Person_PersonType" CHECK (PersonType IS NULL OR UPPER(PersonType) IN ('SC', 'VC', 'IN', 'EM', 'SP', 'GC'))
+  )
 
 -- Crear llaves foraneas 
